5eb71fa7438bda77c411346bb989558132e6449f
Creator-Turbo <bablupandey446@gmail.com>
2025-03-17 16:13:21 +0530

    Data Validation completed

--- src/datascience/entity/config_entity.py
+++ src/datascience/entity/config_entity.py
@@ -9,4 +9,13 @@ class DataIngestionConfig:
     unzip_dir: Path
 
 
+@dataclass
+class DataValidationConfig:
+    root_dir:Path
+    STATUS_FILE:str
+    unzip_data_dir:Path
+    all_schema:dict
+
+
+
     
